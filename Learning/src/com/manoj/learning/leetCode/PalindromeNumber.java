3a8c2f559db9d615a0d54dc93989ed00dcb4dfb4
manojkumars2610 <109852748+manojkumars2610@users.noreply.github.com>
2022-07-25 15:35:38 +0530

    Adjusting for laptop

--- Learning/src/com/manoj/learning/leetCode/PalindromeNumber.java
+++ Learning/src/com/manoj/learning/leetCode/PalindromeNumber.java
@@ -1,7 +1,6 @@
 package com.manoj.learning.leetCode;
-
 public class PalindromeNumber {
-	
+
 	 public boolean isPalindrome(int x) {
 		 int temp=x, y=0;
 		 
@@ -9,20 +8,13 @@ public class PalindromeNumber {
 			 y=y*10+temp%10;
 			 temp=temp/10;
 		 }while(temp>0);
-		 
-		 if (x==y && x>=0) {
-			 return true;
-		 }else
-			 return false;
-	        
-	    }
 
+		 return x == y && x >= 0;
+	    }
 	public static void main(String[] args) {
 		
 		PalindromeNumber myObj = new PalindromeNumber();
-		
 		int x = 0;
-		
 		System.out.println("Output= "+myObj.isPalindrome(x));
 
 	}
